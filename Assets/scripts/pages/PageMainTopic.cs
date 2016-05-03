76e586a635850e0fd9944166ebd63a203ff9ee6a
Shimon Richik <shimon@kinkajoo.co.il>
2016-05-03 19:21:45 +0300

    stamp positioned

--- Assets/scripts/pages/PageMainTopic.cs
+++ Assets/scripts/pages/PageMainTopic.cs
@@ -48,7 +48,7 @@ public class PageMainTopic : BasePage {
         SelectedItemInPosition = new Vector3[6];
         //SelectedItemInPosition = new Vector3(0.1683824f, -0.05769891f, 11.72f);
         SelectedItemInPosition[0] = new Vector3(0.185f, -0.06f, 12.18f);//limo
-        SelectedItemInPosition[1] = new Vector3(0.32f, -0.01f, 12.3f);//stamp
+        SelectedItemInPosition[1] = new Vector3(0.32f, 0.03f, 11.8f);//stamp
         SelectedItemInPosition[2] = new Vector3(0.151f, -0.08f, 12.237f);//knife
         SelectedItemInPosition[3] = new Vector3(0.345f, -0.044f, 11.865f);//system
         SelectedItemInPosition[4] = new Vector3(0.361f, -0.053f, 12.078f);//cabin
